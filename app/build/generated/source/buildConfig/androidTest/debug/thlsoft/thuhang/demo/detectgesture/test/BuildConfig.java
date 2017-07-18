4a46a0c2a49bbea1f0ae07b03c3200bec8bac941
thuhangpham <thuhangpt96@gmail.com>
2017-07-18 11:10:44 +0700

    init commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/thlsoft/thuhang/demo/detectgesture/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package thlsoft.thuhang.demo.detectgesture.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "thlsoft.thuhang.demo.detectgesture.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
